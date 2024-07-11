259be6db3e112e575fcc10df1ae68b638ad2f24a
CamilleCoron <camille.coron@math.u-psud.fr>
2024-07-11 17:39:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,116 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<link rel="shortcut icon" type="image/ico" href="photo2.jpeg" />
+        <title>Camille Coron</title>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-34425590-2', 'u-psud.fr');
+  ga('send', 'pageview');
+
+</script>
+    </head>
+
+    <body>
+		<section id="site">
+			<header>
+				<div id="divtitre"><h1 id="titre">Camille Coron</h1>
+				<p><h3>Ma&icirc;tre de conf&eacute;rences au <a href="http://www.math.u-psud.fr/">Laboratoire de Math&eacute;matiques d'Orsay</a></h3></p>
+
+				</div>
+				<div id="divphoto">
+				<img id="photo" src="photo.jpg" style="height: 5cm; width: auto;" alt="Camille Coron"/>
+				</div>
+			</header>		
+			
+			<section id="corps">
+				<nav>
+					<ul id="blue">
+						<li class="current"><a href="index.php" title="">Accueil<span>Pour aller &agrave; l'accueil</span></a></li>
+						<li><a href="publications.php" title="">Publications et Expos&eacute;s<span></span></a></li>
+						<li><a href="enseignementetautre.php" title="">Enseignement et Autres activit&eacute;s<span></span></a></li>
+						<li><a href="liens.php" title="">Liens<span></span></a></li>
+						<li><a href="index_english.php" title="">English version<span></span></a></li>
+					</ul>
+				</nav>
+				
+				<div id="presentation">
+				<h2>Pr&eacute;sentation</h2>
+					<div id="presentationboite">
+<p>Je suis maintenant professeur junior &agrave; l'<a href="https://www.inrae.fr/">INRAE</a> depuis septembre 2023. Je fais partie de l'&Eacute;quipe <a href="https://mia-ps.inrae.fr/solstis">SOLsTIS</a> du laboratoire <a href="https://mia-ps.inrae.fr/">MIA-Paris-Saclay</a>.</p>
+
+
+					<p>J'ai &eacute;t&eacute; ma&icirc;tresse de conf&eacute;rences &agrave; l'<a href="http://www.u-psud.fr/fr/index.html">Universit&eacute; Paris Sud</a> entre septembre 2014 et septembre 2024. Je faisais partie de l'&Eacute;quipe <a href="http://www.math.u-psud.fr/stats/NEW/stats.php">Probabilit&eacute;s et statistiques</a> du <a href="https://www.imo.universite-paris-saclay.fr/fr/">Laboratoire de Math&eacute;matiques d'Orsay</a>.</p>
+
+<!--<p>J'enseigne et je suis directrice des &eacute;tudes &agrave; l'<a href="http://www.iut-sceaux.u-psud.fr/fr/index.html">IUT de Sceaux</a>.</p>-->
+
+<p>Je m'int&eacute;resse aux probabilit&eacute;s et aux statistiques appliqu&eacute;es
+					&agrave; l'&eacute;cologie. <br>
+Je m'int&eacute;resse en particulier aux r&ocirc;les respectifs de la reproduction mend&eacute;lienne, de la d&eacute;mographie et des diff&eacute;rents modes de reproduction sur l'&eacute;volution darwinienne d'une population. J'&eacute;tudie pour cela des processus de naissance et mort non-lin&eacute;aires et des processus à valeur mesure ainsi que leur convergence sous diff&eacute;rentes &eacute;chelles de temps, de taille de population et de param&egrave;tres d&eacute;mographiques. D'autre part, j'&eacute;tudie actuellement l'estimation et le suivi d'abondances d'esp&egrave;ces &agrave; l'aide de jeux de donn&eacute;es issus de programmes de sciences participatives comme ceux de <a href="http://vigienature.mnhn.fr/‎">Vigie Nature</a> et <a href="www.faune-aquitaine.org/‎">Faune Aquitaine</a>. </p>
+</br>
+					<p>J'ai effectu&eacute; ma <a href="These.pdf">th&egrave;se</a> au <a href="http://www.cmap.polytechnique.fr/">Centre de Math&eacute;matiques Appliqu&eacute;es</a> de 
+					l'&Eacute;cole Polytechnique, sous la direction de 
+					<a href="http://www.cmap.polytechnique.fr/spip.php?rubrique61">
+					Sylvie M&eacute;l&eacute;ard</a>. J'ai occup&eacute; durant un an un poste de <a href="http://www.fondation-hadamard.fr/fr/lecteurs_hadamard">Lectrice Hadamard</a>, au Laboratoire de Math&eacute;matiques d'Orsay.</p>
+					
+					
+					</div>
+				</div>
+				
+				<div id="contact">	
+				<h2>Me contacter</h2>
+					<div id="contactboite">
+					<h3>Camille Coron</h3>
+					<p>Facult&eacute; des Sciences d'Orsay<br/>
+					B&acirc;timent 425, D&eacute;partement de Math&eacute;matiques<br/>
+					91405 Orsay cedex <br/>
+					Bureau 114<br/>
+01 69 15 57 44<br/><br/>
+
+IUT de Sceaux<br/>
+					8 avenue Cauchy
+					92330 Sceaux <br/>
+					Bureau 518<br/>
+01 40 91 24 72<br/><br/>
+					camille.coron AT math.u-psud.fr</p>
+					</div>
+
+			</div>
+
+
+
+
+
+			</section>
+			<footer><center>
+			<p>
+
+			<div id="CSS1">
+				<a href="http://www.math.u-psud.fr/">
+					<img style="border:0;height:50px;"
+						src="logoLMO.jpg"
+						alt="Laboratoire de Math&eacute;matiques d'Orsay" />
+				</a>
+			</div>
+			<div id="CSS1">
+				<a href="http://www.iut-sceaux.u-psud.fr/fr/index.html">
+					<img style="border:0;height:50px;"
+						src="logo_IUT.jpg"
+						alt="IUT de Sceaux" />
+				</a>
+			</div>
+
+			</p>
+			</center></footer>
+		</section>
+	
+		
+
+    </body>
+</html>
