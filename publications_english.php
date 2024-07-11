65831c2dfb8419e8dca1df2bfb7aca6e708ec57d
Camille Coron <camille.coron@math.u-psud.fr>
2024-07-11 19:00:17 +0200

    Add files via upload

---
+++ publications_english.php
@@ -0,0 +1,110 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<link rel="shortcut icon" type="image/ico" href="photo.jpg" />
+        <title>Camille Coron</title>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34425590-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+    </head>
+
+    <body>
+		<section id="site">
+			<header>
+				<div id="divtitre"><h1 id="titre">Camille Coron</h1>
+				<p><h3>Ma&icirc;tre de conf&eacute;rences at <a href="http://www.math.u-psud.fr/">Laboratoire de Math&eacute;matiques d'Orsay</a></h3></p>
+				</div>
+				<div id="divphoto">
+				<img id="photo" src="photo.jpg" style="height: 5cm; width: auto;"  alt="Camille Coron"/>
+				</div>
+			</header>		
+			
+			<section id="corps">
+				<nav>
+					<ul id="blue">
+						<li><a href="index_english.php" title="">Home<span>Pour aller &agrave; l'accueil</span></a></li>
+						<li class="current"><a href="publications_english.php" title="">Publications and Conferences<span></span></a></li>
+						<li><a href="enseignementetautre_english.php" title="">Teaching and other activities<span></span></a></li>
+						<li><a href="liens_english.php" title="">Links<span></span></a></li>
+						<li><a href="publications.php" title="">Version Fran&ccedil;aise<span></span></a></li>
+					</ul>
+				</nav>
+				
+				<div id="texte">
+				<h2>(Pre)publications</h2>
+					<div id="texteboite">
+					<p>
+						<ul>
+<li><a href="https://arxiv.org/abs/2007.15479">Genetics of the biparental Moran model</a> With Yves Le Jan.
+<li><a href="https://www.biorxiv.org/content/early/2017/11/06/214767">Quetzal - an open source C++ template library for coalescence-based environmental demogenetic models inference</a>. Arnaud Becheler, C.C, Stéphane Dupas.</li>
+<li><a href="https://arxiv.org/pdf/1902.07926.pdf">Emergence of homogamy in a two-loci stochastic population model.</a> Avec M. Costa, F. laroche, H. Leman et C. Smadi.</li>
+<li><a href="https://www.biorxiv.org/content/early/2017/10/21/207100">Effects of demographic stochasticity and life-history strategies on times and probabilities to fixation: an individual-based model</a>. With Diala Abu Awad.</li>
+<li><a href="https://arxiv.org/abs/1704.08199">Perpetual integrals convergence and extinctions in population dynamics</a>. With Sylvie M&eacute;l&eacute;ard and Denis Villemonais.</li>
+<li><a href="Habitat.pdf">Estimation of species relative abundances and habitat preferences using opportunistic data</a>. With Clément Calenge, Christophe Giraud and Romain Julliard.</li>
+<li><a href="https://hal.archives-ouvertes.fr/hal-01281975/">A stochastic model for speciation by mating preferences</a>. With Manon Costa, H&eacute;l&egrave;ne Leman et Charline Smadi.</li>
+<li><a href="https://hal.archives-ouvertes.fr/hal-01021396/document">Capitalizing on opportunistic data for monitoring species relative abundances.</a> With Christophe Giraud, Cl&eacute;ment Calenge and Romain Julliard. Biometrics 72 (2), 649-58 (2016).
+<li><a href="ESAIMCoron.pdf"> A model for Mendelian populations demogenetics.</a> To appear in to ESAIM: proceedings.</li>
+<li><a href="Coron2013b.pdf">Slow-fast stochastic diffusion dynamics and quasi-stationary distributions for diploid populations.</a> Journal of Mathematical Biology (Published Online).</li>
+<li><a href="http://projecteuclid.org/download/pdf_1/euclid.aap/1401369702">Stochastic modeling of density-dependent diploid populations and extinction vortex.</a> Adv. Appl. Prob.
+46, 446-477 (2014).</li>		
+<li><a href="Coronetal2013.pdf">Quantifying the mutational meltdown in diploid populations.</a> With <a href="http://www.cmap.polytechnique.fr/spip.php?rubrique61">Sylvie M&eacute;l&eacute;ard</a>,
+<a href="http://www2.mnhn.fr/cersp/spip.php?rubrique28">Emmanuelle Porcher</a>, et <a href="http://www2.mnhn.fr/cersp/spip.php?rubrique17">Alexandre Robert</a>. The American Naturalist, Vol. 181, No. 5 (Mai 2013), pp. 623-636.</li>
+<li><a href="These.pdf">Th&egrave;se: Mod&eacute;lisation probabiliste et &eacute;co-&eacute;volution d'une population diplo&iuml;de.</a>
+						</ul>
+					</p>
+					</div>
+				</div>
+				
+				<div id="texte">	
+				<h2>Quelques expos&eacute;s</h2>
+					<div id="texteboite">
+					<p>
+						<ul>		
+<li>Journ&eacute;es MAS (Toulouse, Ao&ucirc;t 2014): <a href="CoronMAS.pdf">Comportement quasi-stationnaire de populations diplo&iuml;des</a></li>
+<li>International Statistical Ecology Conference (Montpellier, Juillet 2014): <a href="CoronISEC2014.pdf">Estimation d'abondances d'esp&egrave;ces et donn&eacute;es opportunistes</a></li>
+<li>Journ&eacute;es Plantes-Pollinisateurs (Lille, D&eacute;cembre 2012): <a href="exposeLilleCoron.pdf">Mod&eacute;lisation al&eacute;atoire du vortex d'extinction chez une population diplo&iuml;de</a></li>
+<li>Soutenance de th&egrave;se (&Eacute;cole Polytechnique, D&eacute;cembre 2013): <a href="SoutenanceCoron.pdf">Stochastic modeling and eco-evolution
+of a diploid population</a></li>					
+						</ul>
+					</p>
+					</div>
+				</div>
+			</section>
+			<footer><center>
+			<p>
+
+			<div id="CSS1">
+				<a href="http://www.math.u-psud.fr/">
+					<img style="border:0;height:50px;"
+						src="logoLMO.jpg"
+						alt="Laboratoire de Math&eacute;matiques d'Orsay" />
+				</a>
+			</div>
+			<div id="CSS1">
+				<a href="http://www.iut-sceaux.u-psud.fr/fr/index.html">
+					<img style="border:0;height:50px;"
+						src="logo_IUT.jpg"
+						alt="IUT de Sceaux" />
+				</a>
+			</div>
+
+			</p>
+			</center></footer>
+		</section>
+	
+		
+
+    </body>
+</html>
